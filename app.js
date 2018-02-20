5f696b5dc51143c3dcf222254c4f8ce37c1c8ae5
ItaloMFA <italo@codebit.com.br>
2018-02-20 13:19:21 -0300

    dev

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-/**Este arquivo, é basicamente como o ContextListener */
 var app = require('./config/server');
 var rotaNoticias = require('./app/routes/noticias');
 var rotaHome = require('./app/routes/home');
@@ -7,7 +6,7 @@ var rota404 = require('./app/routes/404');
 
 inicializarRotas(app);
 
-function inicializarRotas(app){	
+function inicializarRotas(app){
 	rotaNoticias(app);
 	rotaHome(app);
 	rotaFormularioInclusaoNoticia(app);
@@ -15,6 +14,5 @@ function inicializarRotas(app){
 }
 
 app.listen(3000, function(){
-	console.log('Servidor ON');
+	console.log('Servidor ON - teste');
 });
-///teste
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,16 @@
 module.exports = function(app){
 	app.get('/noticias', function(req, res){
-		res.render("noticias/noticias");
+		var mysql = require('mysql');
+
+		var connection = mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: 'root',
+			database: 'noticias'
+		});//são informados em JSON
+
+		connection.query('select * from noticias', function(erro, resultado){//(sql, callback)
+				res.render("noticias/noticias", {noticias : resultado});//ejs - engine de views
+		});
 	});
 };
